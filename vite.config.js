5c1447c505634b5ea6a3faf1f0d6f5c04792c89f
Zoraluz <zoraluz815@gmail.com>
2026-03-09 22:32:27 -0600

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import * as glob from "glob";
+
+import HtmlCssPurgePlugin from "vite-plugin-purgecss";
+
+function obtenerHtmlFiles(){
+    return Object.fromEntries(
+        glob.sync(
+            './**/*.html',
+            {
+                ignore:[
+                    './dist/**',
+                    './node_modules/**'
+                ]
+            }
+        ).map((file)=>{
+            return [
+                file.slice(0, file.length - file.split('.').pop().length - 1),
+                resolve(__dirname, file)
+            ];
+        })
+    );
+}
+
+export default defineConfig({
+
+    /* necesario para GitHub Pages */
+    base: "/portafolio-vite/",
+
+    build:{
+        rollupOptions:{
+            input: obtenerHtmlFiles()
+        },
+        minify: true
+    },
+
+    css:{
+        preprocessorOptions:{
+            less:{
+                javascriptEnabled: true
+            }
+        }
+    },
+
+    plugins:[
+        HtmlCssPurgePlugin()
+    ]
+
+});
